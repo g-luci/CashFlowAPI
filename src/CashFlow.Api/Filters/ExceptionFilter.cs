7f2254adce078a033be00237757f01420d8bd6d5
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-19 19:15:15 -0300

    Refatorando exceções e filter para atender ao princípio Open-Closed

--- src/CashFlow.Api/Filters/ExceptionFilter.cs
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -24,24 +24,11 @@ namespace CashFlow.Api.Filters
 
         private void HandleProjectException(ExceptionContext context)
         {
-            if (context.Exception is ErrorOnValidationException errorOnValidationException)
-            {
-                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            var cashFlowException = (CashFlowException)context.Exception;
+            var errorResponse = new ResponseErrorJson(cashFlowException.GetErros());
 
-                context.Result = new BadRequestObjectResult(new ResponseErrorJson(errorOnValidationException.Errors));
-            }
-            else if (context.Exception is NotFoundException notFoundException) 
-            {
-                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-
-                context.Result = new NotFoundObjectResult(new ResponseErrorJson(notFoundException.Message));
-            }
-            else
-            {
-                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-
-                context.Result = new BadRequestObjectResult(new ResponseErrorJson(context.Exception.Message));
-            }
+            context.HttpContext.Response.StatusCode = cashFlowException.StatusCode;
+            context.Result = new ObjectResult(errorResponse);
 
         }
 
