1dbe6b5e06187a399d76dbf284600f9befc45fdd
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-20 03:31:55 -0300

    Criando UpdateExpenseUseCase e injetando sua dependência

---
+++ src/CashFlow.Application/UseCases/Expenses/Update/UpdateExpenseUseCase.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using CashFlow.Communication.Requests;
+using CashFlow.Domain.Repositories;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Update
+{
+    public class UpdateExpenseUseCase : IUpdateExpenseUseCase
+    {
+        private readonly IExpensesUpdateOnlyRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateExpenseUseCase(IExpensesUpdateOnlyRepository repository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            
+        }
+
+        public async Task Execute(long id, RequestExpenseJson request)
+        {
+            Validate(request);
+
+            var expense = await _repository.GetById(id);
+
+            if (expense is null)
+            {
+                throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
+            }
+
+            _repository.Update(_mapper.Map(request, expense));
+            //Ou apenas _mapper.Map(request, expense); pois o GetById já está rastreando a entidade.
+
+            await _unitOfWork.Commit();
+        }
+
+        private void Validate(RequestExpenseJson request)
+        {
+            var result = new ExpenseValidator().Validate(request);
+
+            if (!result.IsValid)
+            {
+                throw new ErrorOnValidationException(result.Errors.Select(message => message.ErrorMessage).ToList());
+            }
+        }
+    }
+}
