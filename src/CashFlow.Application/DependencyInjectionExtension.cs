e6815db26e7b88b777ce494f1f31fdb4746d98e8
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-20 02:47:18 -0300

    Criando DeleteExpenseUseCase e injetando sua dependência

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Delete;
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
@@ -28,6 +29,7 @@ namespace CashFlow.Application
             services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
             services.AddScoped<IGetAllExpenseUseCase, GetAllExpenseUseCase>();
             services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase> ();
+            services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
 
             return services;
         }
