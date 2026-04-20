96478456d1cdd62f50623b0af462ca1e1fe358fb
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-20 03:32:34 -0300

    Criando EndPoint Update

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -2,6 +2,7 @@
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Application.UseCases.Expenses.Update;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -64,6 +65,22 @@ namespace CashFlow.Api.Controllers
             await useCase.Execute(id);
 
             return NoContent(); 
+        }
+
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(
+            [FromServices] IUpdateExpenseUseCase useCase,
+            [FromRoute] long id,
+            [FromBody] RequestExpenseJson request)
+        {
+            await useCase.Execute(id, request);
+
+            return NoContent();
         }
     }
 }
