b6387a41d15c4ed11fdb6eb027f303d1fb6c3da6
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-24 03:58:43 -0300

    Automatizando migration ao iniciar a API

---
+++ src/CashFlow.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CashFlow.Infrastructure.Migrations
+{
+    public class DataBaseMigration
+    {
+        public async static Task MigrationDatabase(IServiceProvider serviceProvider)
+        {
+           var dbcontext = serviceProvider.GetRequiredService<CashFlowDbContext>();
+
+            await dbcontext.Database.MigrateAsync();
+        }
+    }
+}
