41973cb2f5d677273f5670005a551fdc17a78b62
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-04-27 23:43:13 -0300

    Adicionando novas mensagens de erro

--- src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
+++ src/CashFlow.Exception/ResourceErrorMessages.Designer.cs
@@ -70,6 +70,33 @@ namespace CashFlow.Exception {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to This email address is already in use..
+        /// </summary>
+        public static string EMAIL_ALREADY_REGISTRED {
+            get {
+                return ResourceManager.GetString("EMAIL_ALREADY_REGISTRED", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The email address cannot be empty..
+        /// </summary>
+        public static string EMAIL_EMPTY {
+            get {
+                return ResourceManager.GetString("EMAIL_EMPTY", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The email address is invalid..
+        /// </summary>
+        public static string EMAIL_INVALID {
+            get {
+                return ResourceManager.GetString("EMAIL_INVALID", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Expenses cannot be for the future..
         /// </summary>
         public static string EXPENSE_CANNOT_FOR_THE_FUTURE {
@@ -84,6 +111,24 @@ namespace CashFlow.Exception {
         public static string EXPENSE_NOT_FOUND {
             get {
                 return ResourceManager.GetString("EXPENSE_NOT_FOUND", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character (for example, !, ?, *, .)..
+        /// </summary>
+        public static string INVALID_PASSWORD {
+            get {
+                return ResourceManager.GetString("INVALID_PASSWORD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The name cannot be empty..
+        /// </summary>
+        public static string NAME_EMPTY {
+            get {
+                return ResourceManager.GetString("NAME_EMPTY", resourceCulture);
             }
         }
         
