e5effa34b91f50a3e1b73336364893113b691213
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-01 22:15:52 -0300

    Criando UseCase para login de usuário e injetando sua dependência.

---
+++ src/CashFlow.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,47 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Domain.Repositories.User;
+using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
+using CashFlow.Exception.ExceptionBase;
+
+namespace CashFlow.Application.UseCases.Login.DoLogin
+{
+    public class DoLoginUseCase : IDoLoginUseCase
+    {
+        private readonly IUserReadOnlyRepository _repository;
+        private readonly IPasswordEncripter _passwordEncripter;
+        private readonly IAccessTokenGenerator _accessTokenGenerator;
+
+        public DoLoginUseCase(
+            IUserReadOnlyRepository repository,
+            IPasswordEncripter passwordEncripter,
+            IAccessTokenGenerator accessTokenGenerator)
+        {
+            _repository = repository;
+            _passwordEncripter = passwordEncripter;
+            _accessTokenGenerator = accessTokenGenerator;
+        }
+
+        public async Task<ResponseRegisteredUserJson> Execute(RequestLoginJson request)
+        {
+            var user = await _repository.GetUserByEmail(request.Email);
+
+            if(user is null)
+            {
+                throw new InvalidLoginException();
+            }
+
+            if(!_passwordEncripter.Verify(request.Password, user.Password))
+            {
+                throw new InvalidLoginException();
+            }
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = user.Name,
+                Token = _accessTokenGenerator.Generate(user)
+            };
+        }
+    }
+}
