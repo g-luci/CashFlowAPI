5b07b0ed5d54d1cf9a174bc2e89d7c354afcb641
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-05 23:56:21 -0300

    Criando Testes para User - Success

---
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,23 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CommonTestUtilities.Requests;
+using Shouldly;
+
+namespace Validators.Tests.Users.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterUserValidator();
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.ShouldBeTrue();
+        }
+    }
+}
