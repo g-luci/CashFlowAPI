213d0fb400282798f16be278b16d06d34247a4aa
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-13 19:01:04 -0300

    Criando Builders para PasswordEncripter, Mapper, UnitOfWork, UserReadRepo, UserWriteRepo e JwtTokenGenerator

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Security.Cryptography;
+using Moq;
+
+namespace CommonTestUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        public static IPasswordEncripter Build()
+        {
+            var mock = new Mock<IPasswordEncripter>();
+
+            mock.Setup(passwrodEncripterConfig => 
+            passwrodEncripterConfig.Encrypt(It.IsAny<string>())).Returns("!$@zchxziuchzxi#duashdi^uash@uhv-cbuyxzhc$");
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tests/CommonTestUtilities/Token/JwtTokenGeneratorBuilder.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Tokens;
+using Moq;
+
+namespace CommonTestUtilities.Token
+{
+    public class JwtTokenGeneratorBuilder
+    {
+        public static IAccessTokenGenerator Build()
+        {
+            var mock = new Mock<IAccessTokenGenerator>();
+                
+            mock.Setup(accessTokenGeneratorConfig => 
+                accessTokenGeneratorConfig
+                .Generate(It.IsAny<User>()))
+                .Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3QiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTE2MjM5MDIyfQ.zVf_WAdU9oOU6ORHGws8Hh7RsobvSzQgo3yOrM7Iht8");
+
+            return mock.Object;
+        }
+    }
+}
