0114233f04bcffe949078e042da10c33dddf1916
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-13 19:01:29 -0300

    Criando testes para Register User

---
+++ tests/UseCases.Test/Users/Register/RegisterUserUserCaseTest.cs
@@ -0,0 +1,76 @@
+﻿using CashFlow.Application.UseCases.Users.Register;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionBase;
+using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using CommonTestUtilities.Token;
+using Shouldly;
+
+namespace UseCases.Test.Users.Register
+{
+    public class RegisterUserUserCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.ShouldNotBeNull();
+            result.Name.ShouldBe(request.Name);
+            result.Token.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public async Task Error_Name_Empty()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            var useCase = CreateUseCase();  
+
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+            result.GetErros().ShouldSatisfyAllConditions(condition_one => condition_one.Count().ShouldBe(1),
+                condition_two => condition_two.ShouldContain(ResourceErrorMessages.NAME_EMPTY));
+        }
+
+        [Fact]
+        public async Task Error_Email_ALready_Exist()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase(request.Email);  
+
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.ShouldThrowAsync<ErrorOnValidationException>();
+
+            result.GetErros().ShouldSatisfyAllConditions(condition_one => condition_one.Count().ShouldBe(1),
+                condition_two => condition_two.ShouldContain(ResourceErrorMessages.EMAIL_ALREADY_REGISTRED));
+        }
+
+        private RegisterUserUseCase CreateUseCase(string? email = null)
+        {
+            var mapper = MapperBuilder.Build();
+            var unitOfWork = UnitOfWorkBuilder.Build();
+            var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+            var passwordEncripter = PasswordEncripterBuilder.Build();
+            var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+            var readRepository = new UserReadOnlyRepositoryBuilder();
+
+            if (string.IsNullOrWhiteSpace(email) == false)
+            {
+                readRepository.ExistActiveUserWithEmail(email);
+            }
+
+            return new RegisterUserUseCase(mapper, passwordEncripter, readRepository.Build(), writeRepository, unitOfWork, tokenGenerator);
+        }
+    }
+}
