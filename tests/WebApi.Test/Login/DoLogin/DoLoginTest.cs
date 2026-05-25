e9268cfa7f026d0fbdf6b1c4f88db85b35530c5e
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-25 15:10:28 -0300

    Criando Testes para o endpoint de Login

---
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,70 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Login.DoLogin
+{
+    public class DoLoginTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private const string METHOD = "api/Login";
+        private readonly HttpClient _httpClient;
+        private readonly string _email;
+        private readonly string _name;
+        private readonly string _password;
+
+        public DoLoginTest(CustomWebApplicationFactory webAppAplicationFactory)
+        {
+            _httpClient = webAppAplicationFactory.CreateClient();
+            _email = webAppAplicationFactory.GetEmail();
+            _name = webAppAplicationFactory.GetName();
+            _password = webAppAplicationFactory.GetPassword();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestLoginJson { Email = _email, Password = _password };
+
+            var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+            var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("name").GetString().ShouldBe(_name);
+            responseData.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Login_Invalid(string culture)
+        {
+            var request = RequestLoginJsonBuilder.Build();
+
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+            var response = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+            var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+            var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("EMAIL_OR_PASSWORD_INVALID", new CultureInfo(culture));
+
+            errors.ShouldSatisfyAllConditions(condition_one => condition_one.Count().ShouldBe(1),
+                condition_two => condition_two.ShouldContain(error => error.GetString()!.Equals(expectedMessage)));
+        }
+    }
+}
