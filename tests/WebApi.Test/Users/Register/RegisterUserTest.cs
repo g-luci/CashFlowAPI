dbeb876e623226a6256d950a08fbf1c0a4c16edf
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-05-25 15:09:45 -0300

    Criando Testes para o endpoint de registro de usuário

---
+++ tests/WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -0,0 +1,65 @@
+﻿using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using Shouldly;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Net.Http.Headers;
+using System.Globalization;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Users.Register
+{
+    public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private const string METHOD = "api/User";
+        private readonly HttpClient _httpClient;
+
+        public RegisterUserTest(CustomWebApplicationFactory webAppAplicationFactory)
+        {
+            _httpClient = webAppAplicationFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            result.StatusCode.ShouldBe(HttpStatusCode.Created);
+
+            var body =  await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            response.RootElement.GetProperty("name").GetString().ShouldBe(request.Name);
+            response.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrEmpty();
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTest))]
+        public async Task Error_Empty_Name(string culture)
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+
+            _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+
+            var result = await _httpClient.PostAsJsonAsync(METHOD, request);
+
+            result.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            var errors = response.RootElement.GetProperty("errorMessages").EnumerateArray();
+
+            var expectedMessage = ResourceErrorMessages.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+            errors.ShouldSatisfyAllConditions(condition_one => condition_one.Count().ShouldBe(1),
+                condition_two => condition_two.ShouldContain(error => error.GetString()!.Equals(expectedMessage)));
+        }
+    }
+}
