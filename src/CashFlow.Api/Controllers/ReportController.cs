9885f0e9dd130dafb993bc5045f49bb3d04c7013
Gabriel Lucas <gabriellucaslc10@gmail.com>
2026-07-29 22:56:20 -0300

    Mudando requisição do ExpenseExcel para Query

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -16,7 +16,7 @@ namespace CashFlow.Api.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         public async Task<IActionResult> GetExcel([FromServices] IGenerateExpensesReportExcelUseCase useCase,
-            [FromHeader] DateOnly month)
+            [FromQuery] DateOnly month)
         {
             byte[] file = await useCase.Execute(month);
 
